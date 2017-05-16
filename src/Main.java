ea3523852a2ea82f82748329591be6b170fe0a6c
goobl1nej <goobl1nej83@gmail.com>
2017-05-16 17:25:45 +0300

    16.05

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,67 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users=generatePhonebook();
-        printPhonebook(users);
-
-        users=loadFromDB();
+//        List<User> users=generatePhonebook();
+        List<User> userList=loadFromDB();
+//        printPhonebook(users);
+        printPhonebook(userList);
     }
 
     private static List<User> loadFromDB() {
         List<User> userList=new ArrayList<>();
-
-
-
+        List<Email> userEmail=new ArrayList<>();
+        List<Phone> userPhone=new ArrayList<>();
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection connectiondb = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","postgres");
+            Statement userstatement = connectiondb.createStatement();
+            ResultSet userset = userstatement.executeQuery("SELECT * FROM user_");
+            while (userset.next()) {
+                int id = userset.getInt("user_id");
+                String lastname = userset.getString("lastname");
+                String firstname = userset.getString("firstname");
+                String middlename = userset.getString("middlename");
+                Date birthday = (userset.getDate("birthday"));
+//                Statement emailstatement = connectiondb.createStatement();
+                ResultSet emailset = userstatement.executeQuery("SELECT * FROM email_");
+                while (emailset.next()) {
+                    if (emailset.getInt("user_id") == id) {
+                        String email = emailset.getString("email");
+                        userEmail.add(new Email(email));
+                    }
+                }
+//                emailset.close();
+//                emailstatement.close();
+//                Statement phonestatement = connectiondb.createStatement();
+                ResultSet phoneset = userstatement.executeQuery("SELECT * FROM phone_");
+                while (phoneset.next()) {
+                    if (phoneset.getInt("user_id") == id) {
+                        String phone = phoneset.getString("phone");
+                        userPhone.add(new Phone(phone));
+                    }
+                }
+//                phoneset.close();
+//                phonestatement.close();
+                userList.add(new User(id, lastname,firstname,middlename,birthday,userEmail,userPhone));
+            }
+//            userset.close();
+//            userstatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return userList;
     }
 
     private static void printPhonebook(List<User> users) {
         for (User person: users) {
-            System.out.println("id: " + person.getId() + "\nФИО: " + person.getFirstname() + " " + person.getLastname() + " " + person.getMiddlename() + "\nДата рождения: " + person.getBirthday());
+            System.out.println("id: " + person.getId() + "\nФИО: " + person.getFirstname() + " " + person.getLastname() + " " + person.getMiddlename() + "\nДата рождения: " + person.getBirthday().getDate()+"."+person.getBirthday().getMonth()+"."+person.getBirthday().getYear());
             if (person.getEmails() != null && !person.getEmails().isEmpty()) {
                 System.out.println("Эл почта:");
                 for (Email email: person.getEmails()) {
@@ -45,7 +87,7 @@ public class Main {
         emailList.add(email);
         ArrayList<Phone> phoneList = new ArrayList<>();
         phoneList.add(phone);
-        users.add(new User(1, "Petr", "Petrov", "Petrovich", "01.12.1978", emailList, phoneList));
+        users.add(new User(1, "Petr", "Petrov", "Petrovich", new Date(1958,12,7), emailList, phoneList));
 
         List<Email> emailList1 = new ArrayList<>();
         emailList1.add(new Email("petrov@da.ru"));
@@ -54,9 +96,9 @@ public class Main {
         phoneList1.add(new Phone("+78324567489"));
         phoneList1.add(new Phone("+78568432246"));
         phoneList1.add(new Phone("+78938432689"));
-        users.add(new User(2, "Ivan", "Petrov", "Sidorovich", "06.09.1965", emailList1, phoneList1));
-        users.add(new User(3, "Test", "Testov", "Testovich", "12.12.2012"));
-        users.add(new User(3, "Test", "Testov", "Testovich", "12.12.2012", new ArrayList<Email>(), new ArrayList<Phone>()));
+        users.add(new User(2, "Ivan", "Petrov", "Sidorovich", new Date(1965,9,6), emailList1, phoneList1));
+        users.add(new User(3, "Test", "Testov", "Testovich", new Date(1958,4,7)));
+        users.add(new User(4, "Test", "Testov", "Testovich", new Date(2012,5,12), new ArrayList<Email>(), new ArrayList<Phone>()));
 
         return users;
     }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class User {
@@ -7,14 +8,14 @@ public class User {
     private String firstname;
     private String lastname;
     private String middlename;
-    private String birthday;
+    private Date birthday;
     private List<Email> emails;
     private List<Phone> phones;
 
     public User() {
     }
 
-    public User(int id, String firstname, String lastname, String middlename, String birthday) {
+    public User(int id, String firstname, String lastname, String middlename, Date birthday) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
@@ -22,7 +23,7 @@ public class User {
         this.birthday = birthday;
     }
 
-    public User(int id, String firstname, String lastname, String middlename, String birthday, List<Email> emails, List<Phone> phones) {
+    public User(int id, String firstname, String lastname, String middlename, Date birthday, List<Email> emails, List<Phone> phones) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
@@ -58,7 +59,7 @@ public class User {
     public void setBirthday() {
         this.birthday=birthday;
     }
-    public String getBirthday() {
+    public Date getBirthday() {
         return birthday;
     }
 
